a34ab8e6290df6636f3de8e7487bf14376e1ee94
Matheus Marchini <matheus@sthima.com.br>
2014-10-19 04:41:42 -0200

    configuring travis ci

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
-import os
-import sys
-
-sys.path.append(os.environ["SUMO_HOME"])
+import os, sys
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
 
 import traci
 
