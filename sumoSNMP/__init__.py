6352c9bced0a8fa3d19033a557aa34b39c7af4ef
Matheus Marchini <matheus@sthima.com.br>
2014-10-20 23:50:36 -0200

    creating a class Sumo

--- sumoSNMP/__init__.py
+++ sumoSNMP/__init__.py
@@ -1,20 +1,26 @@
 import os, sys
-if 'SUMO_HOME' in os.environ:
-    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
-    sys.path.append(tools)
-else:
-    sys.exit("please declare environment variable 'SUMO_HOME'")
 
-import traci
+class Sumo(object):
 
-def hello():
-    PORT = 8813
-    traci.init(PORT)
-    traci.simulationStep()
-    traci.simulationStep()
-    vehicles = traci.vehicle.getIDList()
-    print vehicles
-    traci.close()
+    def __init__(self, port=8813):
 
-    return vehicles
+        if 'SUMO_HOME' in os.environ:
+            tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+            sys.path.append(tools)
+        else:
+            sys.exit("please declare environment variable 'SUMO_HOME'")
+
+        self._traci = __import__("traci")
+
+        self._traci.init(port)
+
+    def close(self):
+        self._traci.close()
+
+    def step(self, steps=1):
+        for i in range(steps):
+            self._traci.simulationStep()
+
+    def get_all_vehicles(self):
+        return self._traci.vehicle.getIDList()
 
--- tests/test-sumo.py
+++ tests/test-sumo.py
@@ -10,6 +10,12 @@ def setup_sumo():
     subprocess.Popen(["sumo", "-c", "tests/fixtures/hello.sumo.cfg"])
 
 @with_setup(setup_sumo)
-def test_hello():
-    assert sumoSNMP.hello() == ["veh0"]
+def test_get_vehicles():
+    sumo = sumoSNMP.Sumo()
+    vehicles = sumo.get_all_vehicles()
+    assert  vehicles == []
+    sumo.step()
+    vehicles = sumo.get_all_vehicles()
+    assert  vehicles == ["veh0"]
+    sumo.close()
 
