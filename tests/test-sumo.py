fe66a7e931597b8ca4a975af84ff34c804bd390e
Matheus Marchini <matheusdot@gmail.com>
2014-11-15 18:34:30 -0200

    improving package architecture

--- tests/test-sumo.py
+++ tests/test-sumo.py
@@ -3,14 +3,14 @@ import subprocess
 
 from nose.tools import with_setup
 
-import sumoSNMP
+from sumoSNMP.sumo import Sumo
 
 def setup_sumo():
     subprocess.Popen(["sumo", "-c", "tests/fixtures/hello.sumo.cfg"])
 
 @with_setup(setup_sumo)
 def test_get_vehicles():
-    with sumoSNMP.Sumo() as sumo:
+    with Sumo() as sumo:
         vehicles = sumo.get_all_vehicles()
         assert  vehicles == []
         sumo.step()
@@ -19,13 +19,13 @@ def test_get_vehicles():
 
 @with_setup(setup_sumo)
 def test_get_traffic_lights():
-    with sumoSNMP.Sumo() as sumo:
+    with Sumo() as sumo:
         traffic_lights = sumo.get_all_traffic_lights()
         assert traffic_lights == ["-125"]
 
 @with_setup(setup_sumo)
 def test_get_traffic_light_state():
-    with sumoSNMP.Sumo() as sumo:
+    with Sumo() as sumo:
         traffic_light = "-125"
         assert sumo.traffic_light_state(traffic_light) == "G"
         sumo.step(16)
