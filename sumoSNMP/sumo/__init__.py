2457726b83d590721f0c69425e0029ee1c50e73d
Matheus Marchini <matheusdot@gmail.com>
2014-11-16 03:37:17 -0200

    Semaphore states setters

--- sumoSNMP/sumo/__init__.py
+++ sumoSNMP/sumo/__init__.py
@@ -38,7 +38,6 @@ class Sumo(object):
         return self._traffic_lights
 
 def get_phase(phases, phase):
-    print filter(lambda p: p._phaseDef.lower()==phase, phases)
     return filter(lambda p: p._phaseDef.lower()==phase, phases).pop()
 
 class TrafficLight(object):
@@ -51,6 +50,10 @@ class TrafficLight(object):
     def _complete_definition(self):
         return self._sumo._traci.trafficlights.getCompleteRedYellowGreenDefinition(self._id)[0]
 
+    @_complete_definition.setter
+    def _complete_definition(self, new_definition):
+        self._sumo._traci.trafficlights.setCompleteRedYellowGreenDefinition(self._id, new_definition)
+
     @property
     def state(self):
         return self._sumo._traci.trafficlights.getRedYellowGreenState(self._id)
@@ -60,12 +63,42 @@ class TrafficLight(object):
         phases = self._complete_definition._phases
         return get_phase(phases, "g")._duration
 
+    @green_duration.setter
+    def green_duration(self, duration):
+        definition = self._complete_definition
+        phase = get_phase(definition._phases, "g")
+        phase_index = definition._phases.index(phase)
+        phase._duration = duration
+        phase._duration2 = duration
+        phase._duration3 = duration
+        self._complete_definition = definition
+
     @property
     def yellow_duration(self):
         phases = self._complete_definition._phases
         return get_phase(phases, "y")._duration
 
+    @yellow_duration.setter
+    def yellow_duration(self, duration):
+        definition = self._complete_definition
+        phase = get_phase(definition._phases, "y")
+        phase_index = definition._phases.index(phase)
+        phase._duration = duration
+        phase._duration2 = duration
+        phase._duration3 = duration
+        self._complete_definition = definition
+
     @property
     def red_duration(self):
         phases = self._complete_definition._phases
         return get_phase(phases, "r")._duration
+
+    @red_duration.setter
+    def red_duration(self, duration):
+        definition = self._complete_definition
+        phase = get_phase(definition._phases, "r")
+        phase_index = definition._phases.index(phase)
+        phase._duration = duration
+        phase._duration2 = duration
+        phase._duration3 = duration
+        self._complete_definition = definition
--- tests/test-sumo.py
+++ tests/test-sumo.py
@@ -2,7 +2,7 @@
 import os
 import subprocess
 
-from nose.tools import with_setup
+from nose.tools import with_setup, raises
 
 from sumoSNMP.sumo import Sumo
 
@@ -20,6 +20,13 @@ def test_get_vehicles():
         sumo.step()
         vehicles = sumo.get_all_vehicles()
         assert  vehicles == ["veh0"]
+
+#TODO Test for route without semaphore
+#TODO Test for semaphore without some colors
+
+#########################
+# Traffic Light Actions #
+#########################
 
 @with_setup(setup_sumo)
 def test_get_traffic_lights():
@@ -39,23 +46,59 @@ def test_get_traffic_light_state():
         sumo.step(51)
         assert traffic_light.state == "G"
 
-#TODO Test for route without semaphore
-#TODO Test for semaphore without some colors
+# Green state get/set duration
 
 @with_setup(setup_sumo)
-def test_green_light_duration():
+def test_get_green_light_duration():
     with Sumo() as sumo:
         traffic_light = traffic_lights = sumo.get_all_traffic_lights()[0]
         assert traffic_light.green_duration == 15000
 
 @with_setup(setup_sumo)
-def test_yellow_light_duration():
+def test_set_green_light_duration():
+    with Sumo() as sumo:
+        duration = 20000
+        traffic_light = traffic_lights = sumo.get_all_traffic_lights()[0]
+        traffic_light.green_duration = duration
+        assert traffic_light.green_duration == duration
+
+# Yellow state get/set duration
+
+@with_setup(setup_sumo)
+def test_get_yellow_light_duration():
     with Sumo() as sumo:
         traffic_light = traffic_lights = sumo.get_all_traffic_lights()[0]
         assert traffic_light.yellow_duration == 3000
 
 @with_setup(setup_sumo)
-def test_red_light_duration():
+def test_set_yellow_light_duration():
+    with Sumo() as sumo:
+        duration = 20000
+        traffic_light = traffic_lights = sumo.get_all_traffic_lights()[0]
+        traffic_light.yellow_duration = duration
+        assert traffic_light.yellow_duration == duration
+
+# Red state get/set duration
+
+@with_setup(setup_sumo)
+def test_get_red_light_duration():
     with Sumo() as sumo:
         traffic_light = traffic_lights = sumo.get_all_traffic_lights()[0]
         assert traffic_light.red_duration == 50000
+
+@with_setup(setup_sumo)
+def test_set_red_light_duration():
+    with Sumo() as sumo:
+        duration = 20000
+        traffic_light = traffic_lights = sumo.get_all_traffic_lights()[0]
+        traffic_light.red_duration = duration
+        assert traffic_light.red_duration == duration
+
+@raises(SystemExit)
+def test_sumo_home_not_defined():
+    """ Test Sumo instantiation without SUMO_HOME environment variable.
+    This test must be run for last, otherwise other tests will fail.
+    """
+    del os.environ["SUMO_HOME"] #TODO find another way to test this without break other tests
+    with Sumo() as sumo:
+        pass
